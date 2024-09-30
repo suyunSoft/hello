0e2c100083629bb276c6760cedeaf5eebb8aae7d
suyunSoft <copilotgpt5@outlook.com>
2024-09-30 23:35:35 +0800

    01

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import base64
 def main(page: ft.Page):
 
     def open_qr_dialog(e):
-        # 加载本地图片并将其转换为 base64 编码
-        with open("/Users/alphabet/Documents/project/GUI/firstgui/assets/example.jpeg", "rb") as image_file:
-            img_base64 = base64.b64encode(image_file.read()).decode('utf-8')
-
         # 创建弹出对话框显示图片
         dialog = ft.AlertDialog(
             title=ft.Text("二维码"),
